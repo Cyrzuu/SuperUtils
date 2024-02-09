223ff92b9ee7ae33800ed8afda154f2ba2775122
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-02-09 19:48:37 +0100

    2.0.0

---
+++ src/main/java/me/cyrzu/git/superutils/NumberUtils.java
@@ -0,0 +1,63 @@
+package me.cyrzu.git.superutils;
+
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@UtilityClass
+public class NumberUtils {
+
+    public static double round(double value, int scale) {
+        if(value <= 0) {
+            return Math.round(value);
+        }
+
+        BigDecimal decimal = BigDecimal.valueOf(value);
+        return decimal.setScale(scale, RoundingMode.FLOOR).doubleValue();
+    }
+
+    public static boolean doubleValue(@NotNull String value) {
+        try {
+            Double.parseDouble(value);
+            return true;
+        } catch (Exception ignore) {
+            return false;
+        }
+    }
+
+    public static double parseDouble(@NotNull String value) {
+        return parseDouble(value, 0D);
+    }
+
+    public static double parseDouble(@NotNull String value, double def) {
+        try {
+            return Double.parseDouble(value);
+        } catch (Exception e) {
+            return def;
+        }
+    }
+
+    public static boolean integerValue(@NotNull String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (Exception ignore) {
+            return false;
+        }
+    }
+
+    public static int parseInteger(@NotNull String value) {
+        return parseInteger(value, 0);
+    }
+
+    public static int parseInteger(@NotNull String value, int def) {
+        try {
+            return Integer.parseInt(value);
+        } catch (Exception e) {
+            return def;
+        }
+    }
+
+}
