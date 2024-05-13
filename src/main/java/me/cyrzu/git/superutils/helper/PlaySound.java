518a14a4dd41f350614094a26e0f964e80d5a8dd
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-05-13 16:11:53 +0200

    2.5.9

--- src/main/java/me/cyrzu/git/superutils/helper/PlaySound.java
+++ src/main/java/me/cyrzu/git/superutils/helper/PlaySound.java
@@ -4,11 +4,19 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public record PlaySound(@NotNull Sound sound, double volume, double pitch) {
+public record PlaySound(@NotNull Sound sound, float volume, float pitch) {
 
-    public static PlaySound ERROR = new PlaySound(Sound.BLOCK_ANVIL_LAND, 0.5, 1.85);
+    public static PlaySound ERROR_1 = new PlaySound(Sound.BLOCK_ANVIL_LAND, 0.5, 1.85);
 
-    public static PlaySound LEVEL_UP = new PlaySound(Sound.ENTITY_PLAYER_LEVELUP, 0.5, 1.25);
+    public static PlaySound ERROR_2 = new PlaySound(Sound.BLOCK_CHEST_OPEN, 0.5, 2);
+
+    public static PlaySound ERROR_3 = new PlaySound(Sound.ENTITY_BLAZE_SHOOT, 0.5, 2);
+
+    public static PlaySound ERROR_4 = new PlaySound(Sound.ENTITY_VILLAGER_NO, 0.5, 0.75);
+
+    public static PlaySound ERROR_5 = new PlaySound(Sound.ENTITY_BAT_HURT, 0.5, 0.85);
+
+    public static PlaySound LEVEL_UP = new PlaySound(Sound.ENTITY_PLAYER_LEVELUP, 0, 1.25);
 
     public static PlaySound XP = new PlaySound(Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 0.5, 1.25);
 
@@ -16,8 +24,20 @@ public record PlaySound(@NotNull Sound sound, double volume, double pitch) {
 
     public static PlaySound CLICK_OFF = new PlaySound(Sound.UI_BUTTON_CLICK, 0.3, 0.75);
 
+    public PlaySound(@NotNull Sound sound, @NotNull Number volume, @NotNull Number pitch) {
+        this(sound, volume.floatValue(), pitch.floatValue());
+    }
+
     public void play(@NotNull Player player) {
-        player.playSound(player.getLocation(), sound, (float) volume, (float) pitch);
+        player.playSound(player.getLocation(), sound, volume, pitch);
+    }
+
+    public void play(@NotNull Player player,  @NotNull Number volume) {
+        player.playSound(player.getLocation(), sound, volume.floatValue(), pitch);
+    }
+
+    public void play(@NotNull Player player,  @NotNull Number volume, @NotNull Number pitch) {
+        player.playSound(player.getLocation(), sound, volume.floatValue(), pitch.floatValue());
     }
 
 }
