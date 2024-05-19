d24b49b50930fcd66337b6d5251fe1d25f6349c2
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-05-19 16:01:47 +0200

    2.7.0

--- src/main/java/me/cyrzu/git/superutils/helper/Scheduler.java
+++ src/main/java/me/cyrzu/git/superutils/helper/Scheduler.java
@@ -37,43 +37,43 @@ public class Scheduler {
         scheduler.runTaskAsynchronously(plugin, task);
     }
 
-    public void later(int delay, @NotNull Runnable task) {
+    public void later(@NotNull Runnable task, int delay) {
         scheduler.runTaskLater(plugin, task, delay);
     }
 
-    public void laterAsync(int delay, @NotNull Runnable task) {
+    public void laterAsync(@NotNull Runnable task, int delay) {
         scheduler.runTaskLaterAsynchronously(plugin, task, delay);
     }
 
-    public void timer(int perioud, @NotNull Runnable task) {
-        this.timer(perioud, t -> task.run());
+    public BukkitTask timer(@NotNull Runnable task, int perioud) {
+        return scheduler.runTaskTimer(plugin, task, 0, perioud);
     }
-    
-    public void timer(int perioud, @NotNull Consumer<BukkitTask> task) {
+
+    public void timer(@NotNull Consumer<BukkitTask> task, int perioud) {
         scheduler.runTaskTimer(plugin, task, 0, perioud);
     }
 
-    public void timer(int perioud, int delay, @NotNull Runnable task) {
-        this.timer(perioud, delay, t -> task.run());
+    public BukkitTask timer(@NotNull Runnable task, int delay, int perioud) {
+        return scheduler.runTaskTimer(plugin, task, delay, perioud);
     }
-    
-    public void timer(int perioud, int delay, @NotNull Consumer<BukkitTask> task) {
+
+    public void timer(@NotNull Consumer<BukkitTask> task, int perioud, int delay) {
         scheduler.runTaskTimer(plugin, task, delay, perioud);
     }
 
-    public void timerAsync(int perioud, @NotNull Runnable task) {
-        this.timerAsync(perioud, t -> task.run());
+    public BukkitTask timerAsync(int perioud, @NotNull Runnable task) {
+        return scheduler.runTaskTimerAsynchronously(plugin, task, 0, perioud);
     }
     
-    public void timerAsync(int perioud, @NotNull Consumer<BukkitTask> task) {
+    public void timerAsync(@NotNull Consumer<BukkitTask> task, int perioud) {
         scheduler.runTaskTimerAsynchronously(plugin, task, 0, perioud);
     }
 
-    public void timerAsync(int perioud, int delay, @NotNull Runnable task) {
-        this.timerAsync(perioud, delay, t -> task.run());
+    public BukkitTask timerAsync(@NotNull Runnable task, int delay, int perioud) {
+        return scheduler.runTaskTimerAsynchronously(plugin, task, delay, perioud);
     }
-    
-    public void timerAsync(int perioud, int delay, @NotNull Consumer<BukkitTask> task) {
+
+    public void timerAsync(@NotNull Consumer<BukkitTask> task, int delay, int perioud) {
         scheduler.runTaskTimerAsynchronously(plugin, task, delay, perioud);
     }
 
