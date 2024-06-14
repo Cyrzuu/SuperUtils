aa9f723a90c4bda44ee0f3a2f7d5a6edae17686e
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-06-14 12:39:29 +0200

    2.9.5

--- src/main/java/me/cyrzu/git/superutils/helper/CooldownManager.java
+++ src/main/java/me/cyrzu/git/superutils/helper/CooldownManager.java
@@ -4,10 +4,12 @@ import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class CooldownManager<T> {
@@ -66,6 +68,21 @@ public class CooldownManager<T> {
         }
 
         this.setCooldown(value, duration);
+    }
+
+    public void removeCooldown(@NotNull T value) {
+        this.cooldownMap.remove(value);
+    }
+
+    public void removeCooldownIf(@NotNull Predicate<T> filter) {
+        Set<T> ts = this.keySet();
+        ts.forEach(key -> {
+            if(!filter.test(key)) {
+                return;
+            }
+
+            this.cooldownMap.remove(key);
+        });
     }
 
     @NotNull
