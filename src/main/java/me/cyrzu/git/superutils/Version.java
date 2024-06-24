bf5ff70a0d7f8f3d0f4dcac8a13c0801c31bbeb6
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-06-24 02:49:24 +0200

    3.0.4

--- src/main/java/me/cyrzu/git/superutils/Version.java
+++ src/main/java/me/cyrzu/git/superutils/Version.java
@@ -5,6 +5,8 @@ import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public enum Version {
     v1_16_R3("1.16.4/1.16.5"),
@@ -18,6 +20,7 @@ public enum Version {
     v1_20_R2("1.20.2"),
     v1_20_R3("1.20.3/1.20.4"),
     v1_20_R4("1.20.5/1.20.6"),
+    v1_21_R1("1.21"),
     UNKNOWN("Unknown");
 
     @Getter
@@ -52,15 +55,40 @@ public enum Version {
         return this.equals(current);
     }
 
+//    @NotNull
+//    private static Version getCurrentVersion() {
+//        String[] bukkitPackage = Bukkit.getServer().getClass().getPackage().getName().split("\\.");
+//        String protocol = bukkitPackage[bukkitPackage.length - 1];
+//
+//        return Arrays.stream(Version.values())
+//                .filter(v -> v.name().equals(protocol))
+//                .findAny()
+//                .orElse(UNKNOWN);
+//    }
+
     @NotNull
     private static Version getCurrentVersion() {
-        String[] bukkitPackage = Bukkit.getServer().getClass().getPackage().getName().split("\\.");
-        String protocol = bukkitPackage[bukkitPackage.length - 1];
-
-        return Arrays.stream(Version.values())
-                .filter(v -> v.name().equals(protocol))
-                .findAny()
-                .orElse(UNKNOWN);
+        String bukkitGetVersionOutput = Bukkit.getVersion();
+        Matcher matcher = Pattern.compile("\\(MC: (?<version>[\\d]+\\.[\\d]+(\\.[\\d]+)?)\\)").matcher(bukkitGetVersionOutput);
+        if (matcher.find()) {
+            return switch (matcher.group("version")) {
+                case "1.16.4", "1.16.5" -> Version.v1_16_R3;
+                case "1.17", "1.17.1" -> Version.v1_17_R1;
+                case "1.18", "1.18.1" -> Version.v1_18_R1;
+                case "1.18.2" -> Version.v1_18_R2;
+                case "1.19", "1.19.1", "1.19.2" -> Version.v1_19_R1;
+                case "1.19.3" -> Version.v1_19_R2;
+                case "1.19.4" -> Version.v1_19_R3;
+                case "1.20", "1.20.1" -> Version.v1_20_R1;
+                case "1.20.2" -> Version.v1_20_R2;
+                case "1.20.3", "1.20.4" -> Version.v1_20_R3;
+                case "1.20.5", "1.20.6" -> Version.v1_20_R4;
+                case "1.21" -> Version.v1_21_R1;
+                default -> UNKNOWN;
+            };
+        } else {
+            throw new RuntimeException("Could not determine Minecraft version from Bukkit.getVersion(): " + bukkitGetVersionOutput);
+        }
     }
 
 }
