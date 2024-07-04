5e20971210383686078ca39128bc6a66c057397c
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-07-04 18:56:13 +0200

    3.1.6

--- src/main/java/me/cyrzu/git/superutils/LocationUtils.java
+++ src/main/java/me/cyrzu/git/superutils/LocationUtils.java
@@ -208,11 +208,26 @@ public class LocationUtils {
     }
 
     public double distance(@Nullable Location location, @Nullable Location secondLocation) {
-        return Math.sqrt(LocationUtils.distanceSquared(location, secondLocation));
+        return LocationUtils.distance(location, secondLocation, false);
     }
 
+    public double distance(@Nullable Location location, @Nullable Location secondLocation, boolean worldEqual) {
+        return Math.sqrt(LocationUtils.distanceSquared(location, secondLocation, worldEqual));
+    }
+
+
     public double distanceSquared(@Nullable Location location, @Nullable Location secondLocation) {
+        return LocationUtils.distanceSquared(location, secondLocation, false);
+    }
+
+    public double distanceSquared(@Nullable Location location, @Nullable Location secondLocation, boolean worldEqual) {
         if(location == null || secondLocation == null) {
+            return Double.MAX_VALUE;
+        }
+
+        World world = location.getWorld();
+        World secondWorld = secondLocation.getWorld();
+        if(worldEqual && (world == null || secondWorld == null || !world.getUID().equals(secondWorld.getUID()))) {
             return Double.MAX_VALUE;
         }
 
