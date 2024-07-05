92e7b3b9762d574808925af074b05588af8be84c
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-07-05 11:53:46 +0200

    3.2.0

--- src/main/java/me/cyrzu/git/superutils/DurationUtils.java
+++ src/main/java/me/cyrzu/git/superutils/DurationUtils.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 public class DurationUtils {
 
     @NotNull
-    public static Duration formatToDuration(String text) {
+    public Duration formatToDuration(String text) {
         try {
             Duration duration = Duration.ZERO;
             String[] parts = text.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
@@ -30,6 +30,34 @@ public class DurationUtils {
         } catch (Exception ignore) {
             return Duration.ZERO;
         }
+    }
+
+    public static String formatToString(@NotNull Duration duration) {
+        return DurationUtils.formatToString(duration.toSeconds());
+    }
+
+    @NotNull
+    public static String formatToString(long time) {
+        long seconds = Math.abs(time);
+
+        long days = seconds / 86400;
+        long hours = (seconds % 86400) / 3600;
+        long minutes = (seconds % 3600) / 60;
+        long secs = seconds % 60;
+
+        if (days > 0) {
+            return String.format("%dd, %dh, %dm, %ds", days, hours, minutes, secs);
+        }
+
+        if (hours > 0) {
+            return String.format("%dh, %dm, %ds", hours, minutes, secs);
+        }
+
+        if (minutes > 0) {
+            return String.format("%dm, %ds", minutes, secs);
+        }
+
+        return String.format("%ds", secs);
     }
 
 }
